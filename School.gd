db37613b5507e15e570ffbb12d9054a961ba375f
Alexandria <alxpettit@gmail.com>
2022-09-09 16:45:57 -0700

    fishe

---
+++ School.gd
@@ -0,0 +1,24 @@
+@tool
+extends MultiMeshInstance3D
+
+@export var runtime_instances: int = 100.0
+@onready var School: MultiMeshInstance3D = %School
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		School.multimesh.instance_count = runtime_instances
+	for i in range(School.multimesh.instance_count):
+		var pos: Transform3D
+		pos = pos.translated(
+			Vector3(randf() * 100 - 50,
+					randf() * 50 - 25,
+					randf() * 50 - 25)
+				);
+		# School.multimesh.set_instance_custom_data(i, )
+		School.multimesh.set_instance_transform(i, pos)
+		School.multimesh.set_instance_custom_data(i, 
+			Color(randf(), randf(), randf(), randf())
+		)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
